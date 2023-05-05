cb5a1c8b25898c3003cf537d2f8d0f65c51b371a
higordevv <pedrohigor2004@gmail.com>
2023-05-05 13:54:37 -0300

    Logo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,38 +1,37 @@
-import { Container } from "@components/defaults";
+import { Container, RootContainer } from "@components/defaults";
 import * as S from "./styled";
 import Link from "next/link";
 import footerLink from "./footerlinks";
 function Footer() {
-    const footerComponent = footerLink.map(value => {
-        return <S.FooterMenuItem>
-            <h3>{value.title}</h3>
-            <ul>
-                {value.links.map((linkValue) => {
-                    return <li>
-                        <Link href={linkValue.link}>
-                            {linkValue.name}
-                        </Link>
-                    </li>
-                })}
-            </ul>
-        </S.FooterMenuItem>
-    })
-    return <>
+  const footerComponent = footerLink.map((value) => {
+    return (
+      <S.FooterMenuItem>
+        <h3>{value.title}</h3>
+        <ul>
+          {value.links.map((linkValue) => {
+            return (
+              <li>
+                <Link href={linkValue.link}>{linkValue.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </S.FooterMenuItem>
+    );
+  });
+  return (
+    <>
+      <RootContainer bgColor="#0031A4">
         <Container>
-            <S.FooterRoot>
+          <S.FooterRoot>
+            <div></div>
 
-                <div>
-
-                </div>
-
-                <S.FooterMenu>
-                    {footerComponent}
-                </S.FooterMenu>
-
-            </S.FooterRoot>
-
+            <S.FooterMenu>{footerComponent}</S.FooterMenu>
+          </S.FooterRoot>
         </Container>
+      </RootContainer>
     </>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/defaults/index.ts
+++ src/components/defaults/index.ts
@@ -7,4 +7,17 @@ const Container = styled.div`
   padding: 0 20px;
 `;
 
-export { Container };
+type RootContainer = {
+  bgColor?: string
+  border?: string
+}
+const RootContainer = styled.div<RootContainer>`
+  width: 100%;
+  max-width: 100vw;
+  margin: 0 auto;
+  background-color: ${(props) => props.bgColor};
+  border: ${(props) => props.bgColor};
+  
+`;
+
+export { Container, RootContainer };
